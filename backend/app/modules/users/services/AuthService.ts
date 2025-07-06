aeca6be60bacb26bba2b46118d73988ff55cabbd
salah2eddin <salaheddin.mohamed12345@gmail.com>
2025-07-06 23:31:35 +0300

    User Auth(Registeration, Login, Logout)

---
+++ backend/app/modules/users/services/AuthService.ts
@@ -0,0 +1,19 @@
+import { Authenticator } from "@adonisjs/auth"
+import User from "../models/User.js"
+import { Authenticators } from "@adonisjs/auth/types"
+
+export default class AuthService{
+  static async login(email: string, password:string, auth: Authenticator<Authenticators>){
+    const user = await User.verifyCredentials(email, password)
+    return await auth.use('api').createToken(user)
+  }
+
+  static async logout(auth: Authenticator<Authenticators>){
+    await auth.use('api').invalidateToken()
+  }
+
+  static async register(userData: { username: string; email: string; password: string }){
+    const user = await User.create(userData)
+    return user
+  }
+}
--- backend/start/routes.ts
+++ backend/start/routes.ts
@@ -9,8 +9,15 @@
 
 import router from '@adonisjs/core/services/router'
 import TicketController from '../app/modules/tickets/controllers/TicketController.js'
+import AuthController from '../app/modules/users/controllers/AuthController.js'
+import { middleware } from './kernel.js'
 
 router.post('/tickets', [TicketController, 'create'])
+router.post('/auth/login', [AuthController, 'login'])
+router.post('/auth/logout', [AuthController, 'logout']).use(
+  middleware.auth({guards: ['api']})
+)
+router.post('/auth/register', [AuthController, 'register'])
 router.get('/', async () => {
   return {
     hello: 'world',
