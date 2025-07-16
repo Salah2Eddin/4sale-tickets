2961c29422ec4ac40c0325c80b10bd56c87233a7
Salah2Eddin <salaheddin.mohamed12345@gmail.com>
2025-07-16 13:17:31 +0300

    fix qr

--- backend/app/modules/tickets/services/qrService.ts
+++ backend/app/modules/tickets/services/qrService.ts
@@ -1,14 +1,15 @@
 import QRCode from 'qrcode'
 import { promises as fs } from 'fs'
 import path from 'path'
+import Ticket from '#modules/tickets/models/Ticket'
 
-export async function generateTicketQR(ticketId: number): Promise<string> {
-  const content = JSON.stringify({id: ticketId})
+export async function generateTicketQR(ticket: Ticket): Promise<string> {
+  const content = JSON.stringify({ id: ticket.id, seat: ticket.seatId, event: ticket.eventId, user: ticket.userId })
 
   const outputDir = path.resolve('./public/qrcodes')
   await fs.mkdir(outputDir, { recursive: true })
 
-  const filePath = path.join(outputDir, `ticket-${ticketId}.png`)
+  const filePath = path.join(outputDir, `ticket-${ticket.id}.png`)
   await QRCode.toFile(filePath, content, {
     errorCorrectionLevel: 'M',
     width: 300,
