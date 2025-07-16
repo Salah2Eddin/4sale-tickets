2f0d6c0e95f41bbbc3fe1e36daa4eae30f270f56
Salah2Eddin <salaheddin.mohamed12345@gmail.com>
2025-07-16 13:19:26 +0300

    added organizerId and earlybird end date to event validators

--- backend/app/modules/events/services/EventService.ts
+++ backend/app/modules/events/services/EventService.ts
@@ -1,31 +1,11 @@
 
 import Event from '#modules/events/models/Event'
-import Seat from '#modules/tickets/models/Seat'
-import Tier from '#modules/tickets/models/Tier'
 
 export default class EventService {
     static async create(data: Partial<Event>) {
     const event = await Event.create(data)
-
-    // await this.createSeatsForEvent(event.id)
-
     return event
     }
-
-  static async createSeatsForEvent(eventId: number) {
-    const tiers = await Tier.query().where('event_id', eventId)
-    for (const tier of tiers) {
-      const seatData = []
-      for (let i = 0; i < tier.capacity; i++) {
-        seatData.push({
-          tierId: tier.id,
-          eventId: eventId,
-        })
-      }
-
-      await Seat.createMany(seatData)
-    }
-  }
 
     static async getAll() {
         return Event.query().preload('organizer')
--- backend/app/modules/events/validators/EventValidators.ts
+++ backend/app/modules/events/validators/EventValidators.ts
@@ -3,6 +3,7 @@ import { DateTime } from 'luxon'
 
 export const createEventValidator = vine.compile(
   vine.object({
+    organizerId: vine.number().positive().withoutDecimals(),
     title: vine.string().minLength(1),
     description: vine.string().minLength(1),
     location: vine.string().minLength(1),
@@ -10,12 +11,14 @@ export const createEventValidator = vine.compile(
     currency: vine.string().minLength(3).maxLength(3),
     startsAt: vine.date().transform((date) => DateTime.fromJSDate(date)),
     endsAt: vine.date().afterField('startsAt', {compare: "hours"}).transform((date) => DateTime.fromJSDate(date)),
+    earlyBirdEndsAt: vine.date().afterField('startsAt', {compare: "hours"}).transform((date) => DateTime.fromJSDate(date)),
     capacity: vine.number().positive().withoutDecimals(),
   })
 )
 
 export const updateEventValidator = vine.compile(
   vine.object({
+    organizerId: vine.number().positive().withoutDecimals(),
     title: vine.string().minLength(1),
     description: vine.string().minLength(1),
     location: vine.string().minLength(1),
@@ -23,6 +26,7 @@ export const updateEventValidator = vine.compile(
     currency: vine.string().minLength(3).maxLength(3),
     startsAt: vine.date().transform((date) => DateTime.fromJSDate(date)),
     endsAt: vine.date().afterField('startsAt').transform((date) => DateTime.fromJSDate(date)),
+    earlyBirdEndsAt: vine.date().afterField('startsAt', {compare: "hours"}).transform((date) => DateTime.fromJSDate(date)),
     capacity: vine.number().positive().withoutDecimals(),
   })
 )
