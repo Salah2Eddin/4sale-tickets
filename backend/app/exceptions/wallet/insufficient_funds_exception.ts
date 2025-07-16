b8c3d84bce3ba2fce865462fdb59e24128e05a8d
Salah2Eddin <salaheddin.mohamed12345@gmail.com>
2025-07-16 13:50:41 +0300

    insufficient funds exception message

--- backend/app/exceptions/wallet/insufficient_funds_exception.ts
+++ backend/app/exceptions/wallet/insufficient_funds_exception.ts
@@ -2,4 +2,5 @@ import { Exception } from '@adonisjs/core/exceptions'
 
 export default class InsufficientFundsException extends Exception {
   static status = 409
+  static message = "Sender wallet has insufficient funds"
 }
